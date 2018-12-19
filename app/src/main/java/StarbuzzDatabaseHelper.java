56ef71b23315a5779628ede18884440a79fece50
andreynho2006 <andreynho2006@gmail.com>
2018-12-19 13:13:13 +0200

    make class to connect to SQL datbase

---
+++ app/src/main/java/StarbuzzDatabaseHelper.java
@@ -0,0 +1,20 @@
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
+
+    StarbuzzDatabaseHelper(Context context) {
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpdate(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
